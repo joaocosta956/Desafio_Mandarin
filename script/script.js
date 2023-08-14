52604018d17e50239b3a200f8fb21e996808e2d9
joaocosta956 <joaovcosta956.com>
2023-08-14 15:12:48 -0300

    deploy Mobile

--- script/script.js
+++ script/script.js
@@ -19,7 +19,6 @@ conteudo.innerHTML+=`
 
 <div class="conteudo-principal">
 <div id="img-principal">
-
 </div>
 <div class="texto-principal">
     <h1>`+postPrincipal.titulo+`</h1>
@@ -28,6 +27,36 @@ conteudo.innerHTML+=`
 </div>
 <span id="color-bar"></span>
 </div>
+<div class="box-sub">
+<div class="sub-conteudos">            
+    <div class="img-sub">
+    </div>
+    <div class="texto-principal">
+        <h1>NRF 2020: conheça os top 5 assuntos do evento.</h1>
+        <h2>O maior evento sobre varejo do mundo mostrando as tendências, oportunidades e inovações do setor.</h2>
+        <h3>Fonte: CMO ADOBE</h3>
+    </div>
+</div>
+<div class="sub-conteudos">            
+    <div class="img-sub">
+    </div>
+    <div class="texto-principal">
+        <h1>NRF 2020: conheça os top 5 assuntos do evento.</h1>
+        <h2>O maior evento sobre varejo do mundo mostrando as tendências, oportunidades e inovações do setor.</h2>
+        <h3>Fonte: CMO ADOBE</h3>
+    </div>
+</div>
+
+<div class="sub-conteudos">            
+    <div class="img-sub">
+</div>
+<div class="texto-principal">
+    <h1>NRF 2020: conheça os top 5 assuntos do evento.</h1>
+    <h2>O maior evento sobre varejo do mundo mostrando as tendências, oportunidades e inovações do setor.</h2>
+    <h3>Fonte: CMO ADOBE</h3>
+</div>
+</div>
+</div> 
 `
 document.getElementById("img-principal").style.backgroundImage = "url("+postPrincipal.imagem+")";
 document.getElementById("color-bar").style.background = "linear-gradient( to right," +postPrincipal.gradient[0]+","+postPrincipal.gradient[1]+")";
